12e025d1c3f2c1e48c2daefe4fce16ac6c661d2f
Michael <michael.adragna@gmail.com>
2021-03-30 11:59:24 +0200

    Game development with TDD

---
+++ game.py
@@ -0,0 +1,25 @@
+"""module docstring"""
+import random
+import string
+
+class Game:
+    """class docstring"""
+
+    def __init__(self):
+        """Init docstring"""
+        self.grid = [random.choice(string.ascii_uppercase) for i in range(9)]
+
+    def is_valid(self, word):
+        """Method docstring"""
+        my_word_is_valid = True
+        grid_copy = self.grid.copy()
+        for letter in word:
+            my_word_is_valid &= letter in grid_copy
+            if my_word_is_valid:
+                grid_copy.remove(letter)
+        return my_word_is_valid
+
+    def extra_useless_method(self):
+        """Method docstring"""
+        # pylint: disable=missing-docstring
+        # pylint: disable=too-few-public-methods
---
+++ tests/test_game.py
@@ -0,0 +1,31 @@
+# tests/test_game.py
+import unittest
+import string
+from game import Game
+
+class TestGame(unittest.TestCase):
+    def test_game_initialization(self):
+        new_game = Game()
+        grid = new_game.grid
+        self.assertIsInstance(grid, list)
+        self.assertEqual(len(grid), 9)
+        for letter in grid:
+            self.assertIn(letter, string.ascii_uppercase)
+
+    def test_game_is_valid(self):
+        new_game = Game()
+        new_game.grid = ["O", "Q", "U", "W", "R", "B", "A", "Z", "E"]
+        word = "BAROQUE"
+        #self.assertIsInstance(grid, Bool)
+        self.assertTrue(new_game.is_valid(word))
+        self.assertEqual(new_game.grid, ["O", "Q", "U", "W", "R", "B", "A", "Z", "E"])
+
+    def test_game_is_invalid(self):
+        new_game = Game()
+        new_game.grid = ["O", "Q", "U", "W", "R", "B", "A", "Z", "E"]
+        word = "BLABLA"
+        self.assertFalse(new_game.is_valid(word))
+        self.assertEqual(new_game.grid, ["O", "Q", "U", "W", "R", "B", "A", "Z", "E"])
+
+
+
